494b651d99d86236b17898ee07e3d10e963ecb8d
Mohammed Noori <kmanoorib@gmail.com>
2019-03-23 02:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  const employee = retrieveEmployeeInformation();
+  // Element.insertAdjacentHTML(position, text)
+  // $("#yo").append("hey hey hey hey!!!!!");
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employee}</li>`);
+}
+
+
+// function eventHandler(event) {
+//   if (event.type == 'fullscreenchange') {
+//     /* handle a full screen toggle */
+//   } else /* fullscreenerror */ {
+//     /* handle a full screen toggle error */
+//   }
+// }
+
+function addNewLiOnClick() {
+  let click = document.querySelector('input[type=submit]');
+  // listen for click so use .addEventListener(listen_to_this, function(event)
+  // .addEventListener(click, function(event)
+  click.addEventListener('click', function(event){addNewElementAsLi()});
+}
+
